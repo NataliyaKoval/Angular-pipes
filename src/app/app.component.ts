39c62343470ac2a109b854b9444673b50670bd4f
NataliiaKoval <hakunata@gmail.com>
2021-03-03 19:09:18 +0200

    titlecase pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+/* tslint:disable:no-trailing-whitespace */
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+
+  onNameChange(value): void {
+    this.name = value;
+  }
 }
